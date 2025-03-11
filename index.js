cc86dbdb29a0b0a66cb2af5d5098af8a9640ee4d
revanthdadi9773 <Revanth.Dadi@winjit.com>
2025-03-11 18:25:33 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const cors = require("cors");
+app.use(cors({ origin: "*" })); // Allow all origins
+
+app.get("/", (req, res) => {
+    res.send("Hello from API!");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
+
